42544c3be88ff7e3e93f0cf739d93bc1d23881c1
dimitar <dimitar.vukman@bluedropmedical.com>
2022-09-20 15:03:58 +0200

    Initial project

---
+++ example/src/test/java/tech/dimitar/liquibase/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.dimitar.liquibase.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
